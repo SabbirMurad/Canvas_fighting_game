f52a576a53deb4cfc09bc68949dc6c45a185e86b
Sabbir Hassan <sbbir0087@gmail.com>
2022-12-23 21:54:04 +0600

    added movements of the players with key press

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ class Sprite{
   update(){
     this.draw();
     this.position.y+=this.velocity.y;
+    this.position.x+=this.velocity.x;
 
     if(this.position.y + this.height + this.velocity.y>=canvas.height){
       this.velocity.y=0;
@@ -58,6 +59,24 @@ const player2 = new Sprite({
   }
 })
 
+//keys
+const keys ={
+  a:{
+    pressed : false
+  },
+  d:{
+    pressed : false
+  },
+  ArrowRight:{
+    pressed : false
+  },
+  ArrowLeft:{
+    pressed : false
+  },
+}
+
+let lastkey;
+let lastkey2;
 //looping animatin
 function animate(){
   window.requestAnimationFrame(animate)
@@ -65,6 +84,22 @@ function animate(){
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
   player1.update();
   player2.update();
+
+  player1.velocity.x = 0 ;
+  if(keys.a.pressed && lastkey ==='a'){
+    player1.velocity.x = -2.5;
+  }
+  else if(keys.d.pressed && lastkey ==='d'){
+    player1.velocity.x = 2.5;
+  }
+
+  player2.velocity.x = 0 ;
+  if(keys.ArrowLeft.pressed && lastkey2 ==='ArrowLeft'){
+    player2.velocity.x = -2.5;
+  }
+  else if(keys.ArrowRight.pressed && lastkey2 ==='ArrowRight'){
+    player2.velocity.x = 2.5;
+  }
 }
 
 animate()
@@ -72,8 +107,42 @@ animate()
 window.addEventListener('keydown',(e)=>{
   switch (e.key){
     case 'd':
-
+    keys.d.pressed = true
+    lastkey = 'd';
     break;
-
+    case 'a':
+    keys.a.pressed = true 
+    lastkey = 'a';
+    break;
+    case 'w' :
+    player1.velocity.y = -10;
+    break;
+    case 'ArrowRight':
+    keys.ArrowRight.pressed = true;
+    lastkey2 = 'ArrowRight';
+    break;
+    case 'ArrowLeft':
+    keys.ArrowLeft.pressed = true;
+    lastkey2 = 'ArrowLeft';
+    break;
+    case 'ArrowUp' :
+    player2.velocity.y = -10;
+    break;
+  }
+})
+window.addEventListener('keyup',(e)=>{
+  switch (e.key){
+    case 'd':
+    keys.d.pressed = false;
+    break;
+    case 'a':
+    keys.a.pressed = false;
+    break;
+    case 'ArrowRight':
+    keys.ArrowRight.pressed = false;
+    break;
+    case 'ArrowLeft':
+    keys.ArrowLeft.pressed = false;
+    break;
   }
 })

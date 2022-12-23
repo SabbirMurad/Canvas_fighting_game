bba18b047e5320f9e21f72841b231280699820dc
Sabbir Hassan <sbbir0087@gmail.com>
2022-12-23 23:53:47 +0600

    added attack box

--- main.js
+++ main.js
@@ -10,20 +10,61 @@ const gravity = 0.7;
 
 //creating the class that will create the players 
 class Sprite{
-  constructor({position,velocity}){
+  constructor({position,velocity,color,offset,canvas}){
+    this.color = color;
     this.position = position;
     this.velocity = velocity;
-    this.height = 150;
+    this.height = canvas.height/3;
+    this.width = canvas.width/20;
     this.lastkey;
+    this.offset = offset
+    this.attackBox = {
+      position :{
+        x:this.position.x,
+        y:this.position.y
+      },
+      width:canvas.width/5,
+      height :canvas.height/10,
+      offset:{
+        x:offset.x,
+        y:offset.y
+      },
+    }
+    this.isAttacking
   }
 
   draw(){
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(this.position.x,this.position.y,50,this.height)
+    //playerBox
+    canvasContext.fillStyle = this.color;
+    canvasContext.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    )
+
+    //attackBox
+    
+    // if(this.isAttacking){
+      canvasContext.fillStyle = "green";
+      canvasContext.fillRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height,
+      )
+    // }
   }
 
   update(){
     this.draw();
+
+    this.attackBox.position.x = this.position.x -this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+
+    this.attackBox.offset.x = this.offset.x;
+    this.attackBox.offset.y = this.offset.y;
+
     this.position.y+=this.velocity.y;
     this.position.x+=this.velocity.x;
 
@@ -33,6 +74,13 @@ class Sprite{
     else{
       this.velocity.y+=gravity
     }
+  }
+
+  attack(){
+    this.isAttacking = true;
+    setTimeout(()=>{
+      this.isAttacking = false;
+    },100)
   }
 }
 
@@ -45,18 +93,36 @@ const player1 = new Sprite({
   velocity:{
     x:0,
     y:0
+  },
+  color:'red',
+  offset:{
+    x:0,
+    y:0
+  },
+  canvas:{
+    width : canvas.width,
+    height : canvas.height
   }
 })
 
 //creating the player 2
 const player2 = new Sprite({
   position:{
-    x:400,
+    x:800,
     y:0
   },
   velocity:{
     x:0,
     y:0
+  },
+  color:'blue',
+  offset:{
+    x:canvas.width/5 -canvas.width/20,
+    y:0
+  },
+  canvas:{
+    width : canvas.width,
+    height : canvas.height
   }
 })
 
@@ -101,12 +167,33 @@ function animate(){
   else if(keys.ArrowRight.pressed && player2.lastkey ==='ArrowRight'){
     player2.velocity.x = 5;
   }
-}
 
+  //detect for collision
+  if( (player1.attackBox.position.x + player1.attackBox.width) >= player2.position.x && 
+      player1.attackBox.position.x <= (player2.position.x + player2.width) &&
+      (player1.attackBox.position.y + player1.attackBox.height) >= player2.position.y &&
+      player1.attackBox.position.y <= (player2.position.y + player2.height) &&
+      player1.isAttacking
+    ){
+    player1.isAttacking = false;
+    console.log("player 2 hit");
+  }
+
+  if( (player2.attackBox.position.x + player2.attackBox.width) >= player1.position.x && 
+      player2.attackBox.position.x <= (player1.position.x + player1.width) &&
+      (player2.attackBox.position.y + player2.attackBox.height) >= player1.position.y &&
+      player2.attackBox.position.y <= (player1.position.y + player1.height) &&
+      player2.isAttacking
+    ){
+    player2.isAttacking = false;
+    console.log("player 1 hit");
+  }
+}
 animate()
 
 window.addEventListener('keydown',(e)=>{
   switch (e.key){
+    //player 1
     case 'd':
     keys.d.pressed = true
     player1.lastkey = 'd';
@@ -118,6 +205,10 @@ window.addEventListener('keydown',(e)=>{
     case 'w' :
     player1.velocity.y = -20;
     break;
+    case ' ' :
+    player1.attack();
+    break;
+    //player 2 
     case 'ArrowRight':
     keys.ArrowRight.pressed = true;
     player2.lastkey = 'ArrowRight';
@@ -128,6 +219,9 @@ window.addEventListener('keydown',(e)=>{
     break;
     case 'ArrowUp' :
     player2.velocity.y = -20;
+    break;
+    case '0':
+    player2.attack();
     break;
   }
 })
